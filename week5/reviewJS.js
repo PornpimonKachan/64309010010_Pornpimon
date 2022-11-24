e58f087b2572283e1f35158a89380ab19c43802b
Chanapa Siriphan <64309010018@tnk.ac.th>
2022-11-24 10:37:16 +0700

    Create reviewJS.js

---
+++ week5/reviewJS.js
@@ -0,0 +1,11 @@
+x = 10;
+var y = 15;
+let myName = 'Baitoey';
+const year = 2022;
+//year = 2023;
+
+console.log("year เป็นชนิด",typeof(year));
+console.log("myName เป็นชนิด",typeof(myName));
+
+y = 'TNK';
+console.log(y, typeof(y));
